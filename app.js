3b1b07b62ec895556d99ae6af3d9439c99bf311c
tiago <tiagofabre@gmail.com>
2017-01-26 20:48:39 -0300

    Starting react-app inside koa project

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const app = new Koa()
 app.use(rootRoutes.routes())
 app.use(rootRoutes.allowedMethods())
 
-app.listen(3000, () => {
-  console.log('Server running at http://localhost:3000')
+app.listen(3001, () => {
+  console.log('Server running at http://localhost:3001')
 })
 
 export default app
